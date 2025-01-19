cbaf5c444e1ea56c99fb5c2988ef506dd3ebd385
flenokel <mrgamersavchenko@gmail.com>
2025-01-19 17:58:05 +0300

    Delete javadocs/reports/apidocs directory

--- javadocs/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"project.asm","l":"project.asm"}];updateSearchResults();
--- javadocs/reports/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"project.asm","l":"Analyzer"},{"p":"project.asm","l":"Launcher"}];updateSearchResults();
